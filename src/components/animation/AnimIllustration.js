35ee7bb246483f498b5fe18a480756c869d062b7
joshuatehrlich <joshuatehrlich@gmail.com>
2025-01-05 15:06:14 -0500

    make everything awesome

---
+++ src/components/animation/AnimIllustration.js
@@ -0,0 +1,52 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".parallax-content", {
+	scrollTrigger: {
+		trigger: '.header',
+		endTrigger: '.parallax-content',
+		start: "0px 0px",
+		end: "bottom top",
+		scrub: true
+	},
+	opacity: 0,
+	y: "15vw",
+	ease: "none"
+});
+
+gsap.to(".parallax-viewport", {
+	scrollTrigger: {
+		trigger: '.header',
+		endTrigger: '.parallax-content',
+		start: "0px 0px",
+		end: "bottom top",
+		scrub: true
+	},
+	backgroundPosition: "50% 0%",
+	ease: "none"
+});
+
+gsap.from(".foot-image", {
+	scrollTrigger: {
+		trigger: '.foot-image',
+		markers: true,
+		start: "top 80%",
+		end: "center 50%",
+		scrub: true
+	},
+	x: -500,
+	opacity: 0
+});
+
+gsap.from(".foot-text", {
+	scrollTrigger: {
+		trigger: '.foot-image',
+		// markers: true,
+		start: "top 80%",
+		end: "center 50%",
+		scrub: true
+	},
+	x: 500,
+	opacity: 0
+});
